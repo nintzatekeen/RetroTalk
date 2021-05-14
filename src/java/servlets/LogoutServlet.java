f83de67d9571fbb714100cdc311784675e93663f
nintzatekeen <09iker05@gmail.com>
2021-05-14 19:34:56 +0200

    Añadido al doGet el logout

--- src/java/servlets/LogoutServlet.java
+++ src/java/servlets/LogoutServlet.java
@@ -23,6 +23,7 @@ public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        doPost(request, response);
     }
 
     @Override
